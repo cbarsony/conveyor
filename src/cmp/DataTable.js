47c41f9eac78cfb18a43ec7becff73fc05fcedde
CsabaBarsony <csaba.barsony@gmail.com>
2020-08-07 19:49:42 +0200

    contact length

--- src/cmp/DataTable.js
+++ src/cmp/DataTable.js
@@ -16,7 +16,8 @@ export const DataTable = ({pulleys, beltSections}) => (
       <th scope="col">Drives</th>
       <th scope="col">Belt length</th>
       <th scope="col">Belt angle</th>
-      <th scope="col">Wrap angle</th>
+      <th scope="col">Contact length</th>
+      <th scope="col">Contact angle</th>
     </tr>
     </thead>
     <tbody>
@@ -28,7 +29,8 @@ export const DataTable = ({pulleys, beltSections}) => (
       const beltLength = getDistanceOfTwoPoints(belt.start, belt.end)
       const beltAngle = getAngleOfTwoPoints(belt.start, belt.end) * 180 / Math.PI
       const prevBeltAngle = getAngleOfTwoPoints(prevBelt.start, prevBelt.end) * 180 / Math.PI
-      const wrapAngle = (beltAngle + (360 - prevBeltAngle)) % 360
+      const contactAngle = (beltAngle + (360 - prevBeltAngle)) % 360
+      const contactLength = (pulley.radius * 2 * Math.PI) / ((Math.PI * 2) / (contactAngle * Math.PI / 180))
 
       return (
         <tr key={pulley.id} className={pulley.isSelected ? 'selectedPulley' : ''}>
@@ -41,7 +43,8 @@ export const DataTable = ({pulleys, beltSections}) => (
           <td>{pulley.type === PULLEY_TYPE.DRIVE_PULLEY ? pulley.driveCount : '-'}</td>
           <td>{beltLength.toFixed(2)}</td>
           <td>{beltAngle.toFixed(2)}&#176;</td>
-          <td>{wrapAngle.toFixed(2)}&#176;</td>
+          <td>{pulley.type === PULLEY_TYPE.POINT_ON_CONVEYOR ? '-' : contactLength.toFixed(2)}</td>
+          <td>{pulley.type === PULLEY_TYPE.POINT_ON_CONVEYOR ? '-' : contactAngle.toFixed(2)}{pulley.type !== PULLEY_TYPE.POINT_ON_CONVEYOR && <span>&#176;</span>}</td>
         </tr>
       )
     })}
